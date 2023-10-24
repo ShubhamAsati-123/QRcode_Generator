12ca1637c96071473c4a4041384939fcb9677361
JokerSA <asatishubham2004@gmail.com>
2023-10-25 00:13:14 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const Form = () => {
         link.download = "my-image-name.png";
         link.href = dataUrl;
         link.click();
-        setLink("")
+        setLink("");
       })
       .catch((err) => {
         console.log(err);
@@ -46,35 +46,52 @@ const Form = () => {
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-black">
-      <div className="h-128 w-128 bg-slate-500 p-5 text-white">
-        <h2 className="text-5xl text-center">QR Code Generator</h2>
+      <div className="lg:h-128 lg:w-128 bg-slate-500 p-5 text-white">
+        <h2 className="text-3xl lg:text-5xl text-center">QR Code Generator</h2>
         <form
           onSubmit={handleSubmit}
           className="flex flex-col gap-5 my-10 mx-5"
         >
           <label htmlFor="Link">Enter the Link</label>
           <input
-            className="p-2 text-xl text-black"
+            className=" text-xl text-black"
             type="text"
             placeholder="Link"
             value={link}
             onChange={(e) => setLink(e.target.value)}
           />
-          <button type="submit">Submit</button>
+          <button
+            type="submit"
+            className="bg-black py-1 bg-opacity-70 hover:bg-opacity-100"
+          >
+            Submit
+          </button>
         </form>
-        <div className="flex justify-center items-center flex-col">
+        <div className="flex justify-center items-center flex-col ">
           {qrCodeValue && (
-            <div
-              className="flex flex-col justify-center items-center"
-              ref={ref}
-            >
-              <QRCode size={192} value={qrCodeValue} viewBox={`0 0 256 256`} className="border-8 rounded-xl"/>
-              <h1 className="text-3xl bg-gray-400 text-black rounded-lg m-5 px-5 py-2">
-                Scan Me
-              </h1>
-            </div>
+            <>
+              <div
+                className="flex flex-col justify-center items-center bg-black rounded-2xl bg-opacity-75 p-5"
+                ref={ref}
+              >
+                <QRCode
+                  size={192}
+                  value={qrCodeValue}
+                  viewBox={`0 0 256 256`}
+                  className="border-8 rounded-xl"
+                />
+                <h1 className="text-3xl bg-gray-400 text-black rounded-lg m-5 px-5 py-2">
+                  Scan Me
+                </h1>
+              </div>
+              <button
+                onClick={onButtonClick}
+                className="mt-2 px-5 py-2 bg-black bg-opacity-70 hover:bg-opacity-100"
+              >
+                Download QR Code
+              </button>
+            </>
           )}
-          <button onClick={onButtonClick}>Download QR Code</button>
         </div>
       </div>
     </div>
